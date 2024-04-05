460ea9d7d7b764352d2db84f82cc85e578b4556d
Computery <77170025+C0mputery@users.noreply.github.com>
2024-04-05 07:27:14 -0400

    Small patch to prevent stuff running 2 times.

--- Patches/MainMenuHandlerPatches.cs
+++ Patches/MainMenuHandlerPatches.cs
@@ -7,44 +7,25 @@ namespace SelfSufficient.Patches
     [HarmonyPriority(Priority.First + 1)]
     internal static class MainMenuHandlerPatches
     {
-
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(MainMenuHandler.Start))]
-        private static void StartPatch()
-        {
-            string? punAppID = SelfSufficient.SelfSufficientConfigFile?.Bind("Settings", "AppID for PUN", "", "The PUN AppID (Only needed for the host)").Value;
-            string? voiceAppID = SelfSufficient.SelfSufficientConfigFile?.Bind("Settings", "AppID for VOICE", "", "The VOICE AppID (Defaults to the PUN AppID)").Value;
-            if (PhotonAppIDUtilities.AttemptAppIDUpdate(punAppID, voiceAppID))
-            {
-                SelfSufficient.SelfSufficientLogger?.LogInfo($"Updated AppIDs to {punAppID} and {voiceAppID}");
-                PhotonAppIDUtilities.PersonalyOverriddenAppIDs = true;
-            }
-
-            if (!PhotonAppIDUtilities.PersonalyOverriddenAppIDs && !PhotonAppIDUtilities.IsUsingDefaultAppIDs())
-            {
-                SelfSufficient.SelfSufficientLogger?.LogInfo("Updating AppIDs to default values as they were not overridden by the user");
-                PhotonAppIDUtilities.AttemptAppIDUpdate(PhotonAppIDUtilities.DefaultPunAppID, PhotonAppIDUtilities.DefaultVoiceAppID);
-            }
-        }
-
         [HarmonyPrefix]
         [HarmonyPatch(nameof(MainMenuHandler.ConnectToPhoton))]
         private static bool ConnectToPhotonPatch()
         {
             string? punAppID = SelfSufficient.SelfSufficientConfigFile?.Bind("Settings", "AppID for PUN", "", "The PUN AppID (Only needed for the host)").Value;
             string? voiceAppID = SelfSufficient.SelfSufficientConfigFile?.Bind("Settings", "AppID for VOICE", "", "The VOICE AppID (Defaults to the PUN AppID)").Value;
+            SelfSufficient.SelfSufficientLogger?.LogInfo($"Trying to updated AppIDs from config.");
             if (PhotonAppIDUtilities.AttemptAppIDUpdate(punAppID, voiceAppID))
             {
-                SelfSufficient.SelfSufficientLogger?.LogInfo($"Updated AppIDs to {punAppID} and {voiceAppID}");
+                SelfSufficient.SelfSufficientLogger?.LogInfo($"Updated AppIDs from config");
                 PhotonAppIDUtilities.PersonalyOverriddenAppIDs = true;
             }
 
-            if (!PhotonAppIDUtilities.PersonalyOverriddenAppIDs && PhotonAppIDUtilities.IsUsingDefaultAppIDs())
+            if (!PhotonAppIDUtilities.PersonalyOverriddenAppIDs && !PhotonAppIDUtilities.IsUsingDefaultAppIDs())
             {
-                SelfSufficient.SelfSufficientLogger?.LogInfo("Updating AppIDs to default values as they were not overridden");
+                SelfSufficient.SelfSufficientLogger?.LogInfo("Updating AppIDs back to default values as they were not overridden by this user");
                 PhotonAppIDUtilities.AttemptAppIDUpdate(PhotonAppIDUtilities.DefaultPunAppID, PhotonAppIDUtilities.DefaultVoiceAppID);
             }
-            return PhotonAppIDUtilities.IsUsingDefaultAppIDs() ? true : false; // We need to skip the original method if we are updating the AppIDs
+            return PhotonAppIDUtilities.IsUsingDefaultAppIDs() ? true : false; // We need to skip the original method if we are updating the AppIDs to our own since we don't got steam auth
         }
     }
 }
